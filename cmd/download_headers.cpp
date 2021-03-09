625c126a2b0f5757880b33a79854b4dc4fd4aa91
Michelangelo Riccobene <michelangelo.riccobene@gmail.com>
2021-03-09 18:09:59 +0100

    Add download_headers implementation (from tg downloader.go)

--- cmd/download_headers.cpp
+++ cmd/download_headers.cpp
@@ -16,11 +16,14 @@
 #include <iostream>
 #include <chrono>
 #include <string>
+#include <vector>
 
 #include <CLI/CLI.hpp>
 #include <grpcpp/grpcpp.h>
+#include <boost/endian/conversion.hpp>
 
 #include <silkworm/db/access_layer.hpp>
+#include <silkworm/db/stages.hpp>
 #include <silkworm/db/util.hpp>
 #include <silkworm/db/tables.hpp>
 #include <silkworm/types/transaction.hpp>
@@ -31,30 +34,130 @@
 
 using namespace silkworm;
 
-class SentryClient {
+// Definitions --------------------------------------------------------------------------------------------------------
+class Hash: public evmc::bytes32 {
   public:
-    SentryClient(std::shared_ptr<grpc::Channel> channel): stub_(sentry::Sentry::NewStub(channel))
-    {
+    using evmc::bytes32::bytes32;
 
+    Hash() {}
+    Hash(ByteView bv) {std::memcpy(bytes, bv.data(), 32);}
+
+    operator Bytes() {return {bytes, 32};}
+    operator ByteView() {return {bytes, 32};}
+
+    uint8_t* raw_bytes() {return bytes;}
+    int length() {return 32;}
+
+    std::string to_hex() { return silkworm::to_hex(*this); }
+    static Hash from_hex(std::string hex) { return Hash(evmc::literals::internal::from_hex<bytes32>(hex.c_str())); }
+
+    static_assert(sizeof(evmc::bytes32) == 32);
+};
+
+using Header = BlockHeader;
+using BlockNum = uint64_t;
+using BigInt = intx::uint256; // use intx::to_string, from_string, ...
+//using Bytes = std::basic_string<uint8_t>; already defined elsewhere
+
+// LMDB access --------------------------------------------------------------------------------------------------------
+class Db {
+    std::shared_ptr<lmdb::Environment> env;
+    std::unique_ptr<lmdb::Transaction> txn;
+
+  public:
+    explicit Db(std::string db_path) {
+        lmdb::DatabaseConfig db_config{db_path};
+        //db_config.set_readonly(false);
+        env = lmdb::get_env(db_config);
+        txn = env->begin_ro_transaction();
     }
 
-    bool setStatus()
-    {
-        grpc::ClientContext context;
-        sentry::StatusData request;
-        google::protobuf::Empty response;
+    std::optional<Hash> read_canonical_hash(BlockNum b) {  // throws db exceptions // todo: add to db::access_layer.hpp?
+        auto header_table = txn->open(db::table::kBlockHeaders);
+        // accessing this table with only b we will get the hash of the canonical block at height b
+        std::optional<ByteView> hash = header_table->get(db::header_hash_key(b));
+        if (!hash) return std::nullopt; // not found
+        assert(hash->size() == kHashLength);
+        return hash.value(); // copy
+    }
 
-        // todo: fill request
-        /*
+    static Bytes head_header_key() { // todo: add to db::util.h?
+        std::string table_name = db::table::kHeadHeader.name;
+        Bytes key{table_name.begin(), table_name.end()};
+        return key;
+    }
+
+    std::optional<Hash> read_head_header_hash() { // todo: add to db::access_layer.hpp?
+        auto head_header_table = txn->open(db::table::kHeadHeader);
+        std::optional<ByteView> hash = head_header_table->get(head_header_key());
+        if (!hash) return std::nullopt; // not found
+        assert(hash->size() == kHashLength);
+        return hash.value(); // copy
+    }
+
+    std::optional<BlockHeader> read_header(BlockNum b, Hash h)  {
+        return db::read_header(*txn, b, h.bytes);
+    }
+
+    std::optional<ByteView> read_rlp_encoded_header(BlockNum b, Hash h)  {
+        auto header_table = txn->open(db::table::kBlockHeaders);
+        std::optional<ByteView> rlp = header_table->get(db::block_key(b, h.bytes));
+        return rlp;
+    }
+
+    static Bytes header_numbers_key(Hash h) { // todo: add to db::util.h?
+        return {h.bytes, 32};
+    }
+
+    std::optional<BlockHeader> read_header(Hash h) { // todo: add to db::access_layer.hpp?
+        auto blockhashes_table = txn->open(db::table::kHeaderNumbers);
+        auto encoded_block_num = blockhashes_table->get(header_numbers_key(h));
+        if (!encoded_block_num) return {};
+        BlockNum block_num = boost::endian::load_big_u64(encoded_block_num->data());
+        return read_header(block_num, h);
+    }
+
+    std::optional<intx::uint256> read_total_difficulty(BlockNum b, Hash h) {
+        return db::read_total_difficulty(*txn, b, h.bytes);
+    }
+
+    BlockNum stage_progress(const char* stage_name) {
+        return db::stages::get_stage_progress(*txn, stage_name);
+    }
+
+};
+
+// SentryClient -------------------------------------------------------------------------------------------------------
+class SentryClient {
+  public:
+    SentryClient(ChainConfig chain, Hash genesis, std::vector<BlockNum> hard_forks, std::shared_ptr<grpc::Channel> channel):
+        chain_{chain}, genesis_(genesis), hard_forks_(std::move(hard_forks)), stub_{sentry::Sentry::NewStub(channel)}
+    {
+    }
+
+    /* Sends a
         message StatusData {
             uint64 network_id = 1;
             bytes total_difficulty = 2;
             bytes best_hash = 3;
             Forks fork_data = 4;
         }
-        */
-        request.set_network_id(1);
-        //request.set_...
+    */
+    bool setStatus(Hash best_hash, BigInt total_difficulty)
+    {
+        grpc::ClientContext context;
+        sentry::StatusData request;
+        google::protobuf::Empty response;
+
+        request.set_network_id(chain_.chain_id);
+        request.set_total_difficulty(intx::to_string(total_difficulty));
+        request.set_best_hash(best_hash.raw_bytes(), best_hash.length());
+
+        sentry::Forks* forks = new sentry::Forks{};
+        forks->set_genesis(genesis_.raw_bytes(), genesis_.length());
+        for(uint64_t block: hard_forks_)
+            forks->add_forks(block);
+        request.set_allocated_fork_data(forks); // take ownership
 
         grpc::Status invocation = stub_->SetStatus(&context, request, &response);
         if (!invocation.ok()) {
@@ -65,12 +168,18 @@ class SentryClient {
         return true;
     }
   private:
+    // chain identification
+    ChainConfig chain_;
+    Hash genesis_;
+    std::vector<BlockNum> hard_forks_;
+
+    // communication stuff
     std::unique_ptr<sentry::Sentry::Stub> stub_;
 };
 
 // Main
 int main(int argc, char* argv[]) {
-    using std::string, std::cout, std::cerr, std::optional;
+    using std::string, std::cout, std::cerr, std::optional, std::vector;
     using namespace std::chrono;
 
     // Command line parsing
@@ -91,9 +200,37 @@ int main(int argc, char* argv[]) {
 
     // Main loop
     try {
+        Db db{db_path};
+
+        // EIP-2124 based chain identity scheme (networkId + genesis + forks)
+        ChainConfig chain = kMainnetConfig;  // todo: parameterize
+        Hash genesis_hash = Hash::from_hex("0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3"); // mainnet genesis hash in hex // todo: parameterize
+        vector<BlockNum> forks; // todo: improve, but how???
+        forks.push_back(*chain.homestead_block);
+        forks.push_back(*chain.tangerine_whistle_block);
+        forks.push_back(*chain.spurious_dragon_block);
+        forks.push_back(*chain.byzantium_block);
+        forks.push_back(*chain.constantinople_block);
+        forks.push_back(*chain.petersburg_block);
+        forks.push_back(*chain.istanbul_block);
+        forks.push_back(*chain.muir_glacier_block);
+        forks.push_back(*chain.berlin_block);
+
+        // Node current status
+        BlockNum head_height = db.stage_progress(db::stages::kBlockBodiesKey);
+        auto head_hash = db.read_canonical_hash(head_height);
+        if (!head_hash) throw std::logic_error("canonical hash at height " + std::to_string(head_height) + " not found in db");
+        std::optional<BigInt> head_td = db.read_total_difficulty(head_height, *head_hash);
+        if (!head_td) throw std::logic_error("total difficulty of canonical hash at height " + std::to_string(head_height) + " not found in db");
+
+        // Connect to p2p sentry
         auto channel = grpc::CreateChannel(sentry_addr, grpc::InsecureChannelCredentials());
-        SentryClient client(channel);
-        bool ok = client.setStatus();
+
+        SentryClient client(chain, genesis_hash, forks, channel);
+
+        // Send Status to p2p sentry
+        bool ok = client.setStatus(*head_hash, *head_td);
+
         return ok;
     }
     catch(std::exception& e) {
